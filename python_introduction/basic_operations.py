2da6bbdbcc00297fd144057dcf0ebe519fa7d73a
anoldknkomo@gmail.com <anoldknkomo@gmail.com>
2024-12-15 12:53:02 +0200

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Perform the arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# Print the results
+print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Product of {number1} and {number2} is {product_result}")
